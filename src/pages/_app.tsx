f5f6e1faa5b42c3294685c68623e63f8f469407d
Teuuz1994 <matheus.rj.ig@gmail.com>
2021-02-23 19:46:09 -0300

    refactor: change project to nextjs

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import GlobalStyles from '../styles/global';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>Move - IT</title>
+      </Head>
+      <Component {...pageProps}/>
+      <GlobalStyles />
+    </>
+  )
+};
+
+export default MyApp;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,7 +6,6 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-
   :root {
     --white: #fff;
     --background: #f2f3f5;
@@ -20,42 +19,34 @@ export default createGlobalStyle`
     --blue-dark: #4953b8;
     --blue-twitter: #2aa9e0;
   }
-
   @media(max-width: 1080px) {
     html: {
       font-size: 93.75%;
     }
   }
-
   @media(max-width: 720px) {
     html: {
       font-size: 87.5%;
     }
   }
-
   .container {
     height: 100vh;
     max-width: 992px;
     margin: 0 auto;
     padding: 2.5rem 2rem;
-
     display: flex;
     flex-direction: column;
   }
-
   body {
     background: var(--background);
     color: var(--text);
   }
-
   body, button, input, textarea {
     font: 400 16px 'Inter', sans-serif;
   }
-
   button {
     cursor: pointer;
   }
-
   a {
     color: inherit;
     text-decoration: none;
